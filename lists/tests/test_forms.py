bd09012b4575756d399c5991263f74152b75b556
machiner123 <igor.atakhanov@gmail.com>
2016-08-23 21:55:12 -0400

    just before writing my own forms to meet new ExistingListItemFormTest

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,14 +1,27 @@
 from django.test import TestCase
-from lists.forms import EMPTY_ITEM_ERROR, ItemForm
 from lists.models import Item, List
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm
+)
 
 class ItemFormTest(TestCase):
+   
     def test_form_renders_item_text_input(self):
+        '''
+        Instantiate ItemForm, validate that the template passed
+        the right information. as_p outputs what the form is, if it 
+        were surrounded in <p> tags
+        '''
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
         self.assertIn('class="form-control input-lg"', form.as_p())
 
     def test_form_validation_for_blank_items(self):
+        '''
+        Intantiate form with black value for text key, make sure
+        is_valid gives expected result, test our error msg in forms.py works
+        '''
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(
@@ -17,9 +30,37 @@ class ItemFormTest(TestCase):
         )
         
     def test_form_save_handles_saving_to_a_list(self):
+        '''
+        Instantiate and save List obect, instantiate form object
+        with data 'text': 'do me', use form.save to save item to
+        our created list, validate its data
+        '''
         list_ = List.objects.create()
         form = ItemForm(data={'text': 'do me'})
         new_item = form.save(for_list=list_)
         self.assertEqual(new_item, Item.objects.first())
         self.assertEqual(new_item.text, 'do me')
         self.assertEqual(new_item.list, list_)
+        
+class ExistingListItemFormTest(TestCase):
+
+    def test_form_renders_item_text_input(self):
+        
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_)
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+
+
+    def test_form_validation_for_blank_items(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+
+
+    def test_form_validation_for_duplicate_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='no twins!')
+        form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])

596a42fe29148bc7c49b389ec0c34b184918d722
machiner123 <igor.atakhanov@gmail.com>
2016-08-31 15:09:03 -0400

    adding accounts app to master branch now, deleted extra requirements.txt files

---
+++ lists/static/lists.js
@@ -0,0 +1,12 @@
+/*jQuery(document).ready(function ($) {
+    $("input[id='texties']").on('keypress', function () {
+        $('.has-error').hide();
+    });
+});
+*/
+jQuery(document).ready(function ($) {
+    $("input#id_text").on('keypress',function () {
+        $('.has-error').hide();
+    });
+});
+

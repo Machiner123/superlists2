3558e67208037dea7abf240e94b8b60aa3cb7af9
machiner123 <igor.atakhanov@gmail.com>
2016-09-10 17:16:02 -0400

    User model finished, login capability complete, unit tests pass, func tests pass, custom authentication backend complete

--- accounts/tests/test_authentication.py
+++ accounts/tests/test_authentication.py
@@ -1,3 +1,4 @@
+
 from unittest.mock import patch
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -71,8 +72,40 @@ class AuthenticateTest(TestCase):
         self.assertEqual(found_user, actual_user)
         
     def test_creates_new_user_if_necessary_for_valid_assertion(self, mock_post):
+        '''
+        1: Persona response object with stats: ok and user json object for valid assertion
+        2: Run authenticate on persona object
+        3: Retrive user by email, assuming User.objects.create() was run
+        4: Validate that create() was run with proper user data
+        '''
         mock_post.return_value.json.return_value = {'status': 'okay', 'email': 'a@b.com'}
         found_user = self.backend.authenticate('an assertion')
         new_user = User.objects.get(email='a@b.com')
         self.assertEqual(found_user, new_user)
+
+
+class GetUserTest(TestCase):
+
+    def test_gets_user_by_email(self):
+        '''
+        1: Create dummy user 
+        2: Create new user by email
+        3: Retrieve new user by email by passing his email to get_user()
+        4: Test that created new user and retrieved new user are same
+        '''
+        backend = PersonaAuthenticationBackend()
+        other_user = User(email='other@user.com')
+        other_user.username = 'otheruser'
+        other_user.save() # 1
+        desired_user = User.objects.create(email='a@b.com') # 2
+        found_user = backend.get_user('a@b.com') # 3
+        self.assertEqual(found_user, desired_user) # 4
+
+
+    def test_returns_none_if_no_user_with_that_email(self):
+        backend = PersonaAuthenticationBackend()
+        self.assertIsNone(
+            backend.get_user('a@b.com')
+        )
+
 
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -5,6 +5,13 @@ import time
 class LoginTest(FunctionalTest):
 
     def test_login_with_persona(self):
+        '''
+        1: Pass 'Mozila Persona' to switch_to_window, where it will check
+        2: authentication_email is persona's generated id
+        3: We put in edieth's email
+        4: id_logout is generated because she is logged in
+        
+        '''
         # Edith goes to the awesome superlists site
         # and notices a "Sign in" link for the first time.
         self.browser.get(self.server_url)
@@ -16,7 +23,7 @@ class LoginTest(FunctionalTest):
         # Edith logs in with her email address
         ## Use mockmyid.com for test email
         self.browser.find_element_by_id(
-            'authentication_email'  #2
+            'authentication_email'  # 2
         ).send_keys('edith@mockmyid.com') #3
         self.browser.find_element_by_tag_name('button').click()
 
@@ -26,14 +33,19 @@ class LoginTest(FunctionalTest):
         # She can see that she is logged in
         self.wait_for_element_with_id('id_logout')  #4
         navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
+        self.assertIn('edith@mockmyid.com', navbar.text) 
 
     def switch_to_new_window(self, text_in_title):
+        '''
+        1: selenium stores multiple windows in window_handles, iterate through them
+        2: pass each to selenium's own function to switch to a particular window
+        3: check window for title we passed to this function from test_login, return
+        '''
         retries = 20
         while retries > 0:
-            for handle in self.browser.window_handles:
-                self.browser.switch_to_window(handle)
-                if text_in_title in self.browser.title:
+            for handle in self.browser.window_handles: # 1
+                self.browser.switch_to_window(handle) # 2
+                if text_in_title in self.browser.title: # 3
                     return
             retries -= 1
             time.sleep(0.5)

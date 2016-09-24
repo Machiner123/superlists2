58688404f926df10b295a7767eab232991df84e6
Machiner123 <embiggenthyself324726@gmail.com>
2016-09-23 21:56:02 -0400

    just switched order of tests to see if func test on stage changes to help with persona working on func tests but not real tests bug

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -8,18 +8,6 @@ from .base import FunctionalTest
 
 class MyListsTest(FunctionalTest):
 
-    def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        email = 'edith@example.com'
-
-        self.browser.get(self.server_url)
-        self.wait_to_be_logged_out(email)
-
-        # Edith is a logged-in user
-        self.create_pre_authenticated_session(email)
-
-        self.browser.get(self.server_url)
-        self.wait_to_be_logged_in(email)
-
 
     def create_pre_authenticated_session(self, email):
         user = User.objects.create(email=email)
@@ -36,3 +24,14 @@ class MyListsTest(FunctionalTest):
             path='/',
         ))
 
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        email = 'edith@example.com'
+
+        self.browser.get(self.server_url)
+        self.wait_to_be_logged_out(email)
+
+        # Edith is a logged-in user
+        self.create_pre_authenticated_session(email)
+
+        self.browser.get(self.server_url)
+        self.wait_to_be_logged_in(email)

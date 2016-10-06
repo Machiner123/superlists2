cb7f27cb4fe46815bd92639c11306e6e2cf2a7f9
whimsical <igor.atakhanov@gmail.com>
2016-10-06 12:41:13 -0400

    Refactored integrated tests slightly and got rid of redundancies, renamed instances of new_list2 to new_list

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -31,18 +31,7 @@ class ItemFormTest(TestCase):
             [EMPTY_ITEM_ERROR]
         )
         
-    def test_form_save_handles_saving_to_a_list(self):
-        '''
-        Instantiate and save List obect, instantiate form object
-        with data 'text': 'do me', use form.save to save item to
-        our created list, validate its data
-        '''
-        list_ = List.objects.create()
-        form = ItemForm(data={'text': 'do me'})
-        new_item = form.save(for_list=list_)
-        self.assertEqual(new_item, Item.objects.first())
-        self.assertEqual(new_item.text, 'do me')
-        self.assertEqual(new_item.list, list_)
+    
         
 class ExistingListItemFormTest(TestCase):
 

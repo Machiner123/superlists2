2ec20bdb3fd355b0e3e4ea091d43a0c107f7e911
Machiner123 <embiggenthyself324726@gmail.com>
2016-10-10 17:42:06 -0400

    fixed can't find link txt='reticlate spline'

--- lists/models.py
+++ lists/models.py
@@ -1,15 +1,18 @@
-from django.db import models
-from django.core.urlresolvers import reverse
 from django.conf import settings
-
+from django.core.urlresolvers import reverse
+from django.db import models
 
 
 class List(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
 
-  
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
+
+
+    @property
+    def name(self):
+        return self.item_set.first().text
 
 
     @staticmethod
@@ -18,20 +21,16 @@ class List(models.Model):
         Item.objects.create(text=first_item_text, list=list_)
         return list_
 
-    @property
-    def name(self):
-        return self.item_set.first().text
 
-    
+
 class Item(models.Model):
-    text = models.TextField(default="",)
+    text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
-    
+
     class Meta:
-        ordering = ("id",)
-        unique_together = ('list', 'text') 
-        
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+
+
     def __str__(self):
         return self.text
-    
-    
